b28fb6bee19834b1579b3936f517e08edd6ae8d5
cristian <cristianfigueroa19@hotmail.com>
2023-03-27 01:40:25 -0500

    primer commit

---
+++ database/migrations/2023_03_12_212256_create_organizacion_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('organizacion', function (Blueprint $table) {
+            $table->id();
+            $table->string('ruc')->unique();
+            $table->string('razon_social')->unique();
+            $table->string('titulo');
+            $table->string('slogan');
+            $table->text('descripcion');
+            $table->string('director');
+            $table->string('direccion');
+            $table->string('email');
+            $table->string('telefono');
+            $table->text('quienes_somos');
+            $table->text('vision');
+            $table->text('mision');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('organizacion');
+    }
+};
